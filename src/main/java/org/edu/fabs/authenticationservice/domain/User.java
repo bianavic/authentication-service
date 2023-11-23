227781c884d3485422ead72d8dbe7a3e38f50a58
bianavic <bianavic@gmail.com>
2023-11-23 09:26:40 -0300

    infra: create connection to postgresql database

---
+++ src/main/java/org/edu/fabs/authenticationservice/domain/User.java
@@ -0,0 +1,42 @@
+package org.edu.fabs.authenticationservice.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Table(name = "USERS")
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(unique = true, length = 100, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+}
---
+++ src/main/java/org/edu/fabs/authenticationservice/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package org.edu.fabs.authenticationservice.repository;
+
+import org.edu.fabs.authenticationservice.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
